a33f7136418cf187c6c8a4bfa0f025848e2e5e2b
242510292-cloud <242510292@smkn4-tsm.sch.id>
2026-09-03 11:12:06 +0700

    Add files via upload

---
+++ storage/framework/views/014ac3d3a38ed6a1926e83a78a7c6e06.php
@@ -0,0 +1,142 @@
+
+
+<?php $__env->startSection('title', 'Detail Produk'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<?php echo $__env->make('layouts.navbar', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+
+<h2>Detail Produk</h2>
+
+<div class="card">
+    <div class="card-body">
+
+        
+        <?php if($produk->foto): ?>
+            <img
+                src="<?php echo e(asset('storage/' . $produk->foto)); ?>"
+                width="200"
+                class="img-thumbnail mb-3"
+                alt="<?php echo e($produk->nama); ?>"
+            >
+        <?php else: ?>
+            <p class="text-muted">
+                Tidak ada foto
+            </p>
+        <?php endif; ?>
+
+
+        <table class="table table-bordered">
+
+            
+            <tr>
+                <th width="200">ID</th>
+                <td>
+                    <?php echo e($produk->id); ?>
+
+                </td>
+            </tr>
+
+
+
+
+
+            
+            <tr>
+                <th>Nama</th>
+                <td>
+                    <?php echo e($produk->nama); ?>
+
+                </td>
+            </tr>
+
+
+          
+<tr>
+    <th>Jenis</th>
+    <td>
+        <?php echo e($produk->jenisProduk?->nama ?? '-'); ?>
+
+    </td>
+</tr>
+
+
+            
+            <tr>
+                <th>Harga Beli</th>
+                <td>
+                    Rp <?php echo e(number_format($produk->harga_beli, 0, ',', '.')); ?>
+
+                </td>
+            </tr>
+
+
+            
+            <tr>
+                <th>Harga Jual</th>
+                <td>
+                    Rp <?php echo e(number_format($produk->harga_jual, 0, ',', '.')); ?>
+
+                </td>
+            </tr>
+
+
+            
+            <tr>
+                <th>Stok</th>
+                <td>
+                    <?php echo e($produk->stok); ?>
+
+                </td>
+            </tr>
+
+
+            
+            <tr>
+                <th>Dibuat</th>
+                <td>
+                    <?php echo e($produk->created_at?->format('d-m-Y H:i:s')); ?>
+
+                </td>
+            </tr>
+
+
+            
+            <tr>
+                <th>Diupdate</th>
+                <td>
+                    <?php echo e($produk->updated_at?->format('d-m-Y H:i:s')); ?>
+
+                </td>
+            </tr>
+
+        </table>
+
+
+        <div class="mt-3">
+
+            <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update', $produk)): ?>
+
+                <a
+                    href="<?php echo e(route('produk.edit', $produk)); ?>"
+                    class="btn btn-info text-white"
+                >
+                    Edit
+                </a>
+
+            <?php endif; ?>
+
+            <a
+                href="<?php echo e(route('produk.index')); ?>"
+                class="btn btn-secondary"
+            >
+                Kembali
+            </a>
+
+        </div>
+
+    </div>
+</div>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\pos_dezqiyaa\resources\views/produk/show.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/20ab7440dfec98ec4a5b11ec45d23758.php
@@ -0,0 +1,261 @@
+<?php echo csrf_field(); ?>
+
+
+<?php if(!empty($produk->foto)): ?>
+    <div class="mb-3">
+        <label class="form-label">Foto Saat Ini</label><br>
+
+        <img src="<?php echo e(asset('storage/' . $produk->foto)); ?>"
+             width="150"
+             height="150"
+             class="img-thumbnail"
+             style="object-fit: cover"
+             alt="Foto <?php echo e($produk->nama); ?>">
+    </div>
+<?php endif; ?>
+
+<div class="row">
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Gambar</label>
+
+        <input type="file"
+               name="foto"
+               onchange="previewImage(this)"
+               class="form-control <?php $__errorArgs = ['foto'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+               accept="image/*">
+
+        <?php $__errorArgs = ['foto'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback d-block">
+                <?php echo e($message); ?>
+
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Preview Foto</label><br>
+
+        <img id="preview"
+             class="img-thumbnail mt-2"
+             style="display: none; width: 150px; height: 150px; object-fit: cover;"
+             alt="Preview foto">
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Nama Produk</label>
+
+        <input type="text"
+               name="name"
+               class="form-control <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+               value="<?php echo e(old('name', $produk->nama ?? '')); ?>">
+
+        <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback">
+                <?php echo e($message); ?>
+
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">
+            Jenis Produk
+        </label>
+
+        <select name="jenis_produk_id"
+                class="form-select <?php $__errorArgs = ['jenis_produk_id'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>">
+
+            <option value="">
+                -- Pilih Jenis --
+            </option>
+
+            <?php $__currentLoopData = $jenisProduks; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $jenis): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <option value="<?php echo e($jenis->id); ?>"
+                    <?php echo e(old('jenis_produk_id', $produk->jenis_produk_id ?? '') == $jenis->id ? 'selected' : ''); ?>>
+                    <?php echo e($jenis->nama); ?>
+
+                </option>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+        </select>
+
+        <?php $__errorArgs = ['jenis_produk_id'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback">
+                <?php echo e($message); ?>
+
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Harga Beli</label>
+
+        <input type="number"
+               name="purchase_price"
+               class="form-control <?php $__errorArgs = ['purchase_price'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+               value="<?php echo e(old('purchase_price', $produk->harga_beli ?? '')); ?>">
+
+        <?php $__errorArgs = ['purchase_price'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback">
+                <?php echo e($message); ?>
+
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Harga Jual</label>
+
+        <input type="number"
+               name="selling_price"
+               class="form-control <?php $__errorArgs = ['selling_price'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+               value="<?php echo e(old('selling_price', $produk->harga_jual ?? '')); ?>">
+
+        <?php $__errorArgs = ['selling_price'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback">
+                <?php echo e($message); ?>
+
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+    
+    <div class="col-12 mb-3">
+        <label class="form-label">Stok</label>
+
+        <input type="number"
+               name="stock"
+               class="form-control <?php $__errorArgs = ['stock'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+               value="<?php echo e(old('stock', $produk->stok ?? '')); ?>">
+
+        <?php $__errorArgs = ['stock'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+            <div class="invalid-feedback">
+                <div class="invalid-feedback">
+                    <?php echo e($message); ?>
+
+                </div>
+            </div>
+        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+    </div>
+
+</div>
+
+
+<button class="btn btn-success mt-2" type="submit">
+    Simpan
+</button>
+
+<a href="<?php echo e(route('produk.index')); ?>"
+   class="btn btn-secondary mt-2">
+    Kembali
+</a>
+
+
+<script>
+    function previewImage(input) {
+
+        const preview = document.getElementById('preview');
+        const file = input.files[0];
+
+        if (file) {
+            preview.src = URL.createObjectURL(file);
+            preview.style.display = 'block';
+        } else {
+            preview.src = '';
+            preview.style.display = 'none';
+        }
+    }
+</script>
+
+<?php /**PATH C:\laragon\www\pos_dezqiyaa\resources\views/Produk/_form.blade.php ENDPATH**/ ?>
